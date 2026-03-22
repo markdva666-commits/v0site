f4c710ba3bad24ae0bbfb9501cbe6a0438098c1a
Agapitov.Kirill <agapitov.kirill@MacBook-Air-AgapitovKirill.local>
2026-03-22 17:16:04 +0500

    feat: add image for gidronasosy gidromotory category

---
+++ scripts/012-set-gidronasosy-gidromotory-image.sql
@@ -0,0 +1,4 @@
+-- Set catalog image for the root "Гидронасосы | Гидромоторы" category.
+UPDATE categories
+SET image_url = '/gidronasosy-gidromotory-category.png'
+WHERE slug = 'gidronasosy-gidromotory';
